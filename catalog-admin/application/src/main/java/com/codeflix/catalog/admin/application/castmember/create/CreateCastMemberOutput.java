05f7be2eb4771cbc6d1000c61a4fad05f1414bc5
Matheus Vidal <matheusvidal140@gmail.com>
2025-12-03 15:08:09 -0300

    test: Adiciona testes para a API de CastMember

--- catalog-admin/application/src/main/java/com/codeflix/catalog/admin/application/castmember/create/CreateCastMemberOutput.java
+++ catalog-admin/application/src/main/java/com/codeflix/catalog/admin/application/castmember/create/CreateCastMemberOutput.java
@@ -1,13 +1,14 @@
 package com.codeflix.catalog.admin.application.castmember.create;
 
 import com.codeflix.catalog.admin.domain.castmember.CastMember;
+import com.codeflix.catalog.admin.domain.castmember.CastMemberID;
 
 public record CreateCastMemberOutput(
         String id
 ) {
 
-    public static CreateCastMemberOutput from(final String anId) {
-        return new CreateCastMemberOutput(anId);
+    public static CreateCastMemberOutput from(final CastMemberID anId) {
+        return new CreateCastMemberOutput(anId.getValue());
     }
 
     public static CreateCastMemberOutput from(final CastMember aMember) {
--- catalog-admin/application/src/main/java/com/codeflix/catalog/admin/application/castmember/update/UpdateCastMemberOutput.java
+++ catalog-admin/application/src/main/java/com/codeflix/catalog/admin/application/castmember/update/UpdateCastMemberOutput.java
@@ -1,13 +1,14 @@
 package com.codeflix.catalog.admin.application.castmember.update;
 
 import com.codeflix.catalog.admin.domain.castmember.CastMember;
+import com.codeflix.catalog.admin.domain.castmember.CastMemberID;
 
 public record UpdateCastMemberOutput(
         String id
 ) {
 
-    public static UpdateCastMemberOutput from(final String anId) {
-        return new UpdateCastMemberOutput(anId);
+    public static UpdateCastMemberOutput from(final CastMemberID anId) {
+        return new UpdateCastMemberOutput(anId.getValue());
     }
 
     public static UpdateCastMemberOutput from(final CastMember aMember) {
